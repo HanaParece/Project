8de8bdcc44b6386f44fe4b6b57662c4ff6863acc
chungrs <54409983+chungrs@users.noreply.github.com>
2020-09-14 13:33:46 -0400

    messed up the code

--- MBTIdemo.java
+++ MBTIdemo.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class MBTIdemo {
-    public static void main(String[] args) {
+    public static void srdfiucfghgvfdsklxcjfhg(String[] args) {
         int e = 0, i = 0, s = 0, n = 0, t = 0, f = 0, j = 0, p = 0;
         char sex, response;
         
