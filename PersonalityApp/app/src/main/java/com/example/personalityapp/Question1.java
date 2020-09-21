3025835b729102f0e9faefa595ce3cebef4408b2
chungrs <54409983+chungrs@users.noreply.github.com>
2020-09-21 18:48:39 -0400

    incorporated first few questions into android app

--- PersonalityApp/app/src/main/java/com/example/personalityapp/Question1.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Question1.java
@@ -8,43 +8,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class Question1 extends AppCompatActivity {
-    private Button button;                                     //declare buttonA as a button that will be defined
+    private Button buttonA;                                     //declare buttonA as a button that will be defined
     private Button buttonB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question1);                //set to the current page
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonA = (Button) findViewById(R.id.buttonA);
+        buttonA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {                        //define what to do on click
                 //do something
-                openQuestion2();                    //go to next question
+                openPart1();                    //go to next question
             }
         });
-
 
         buttonB = (Button) findViewById(R.id.buttonB);
         buttonB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //do something
-                openQuestion2();                    //go to next question
+                openPart1();                    //go to next question
             }
         });
-
-
-
-
-
-
-
     }
 
 
-    public void openQuestion2(){
-        Intent intent = new Intent(this, Question2.class);
+    public void openPart1(){
+        Intent intent = new Intent(this, Part1.class);
         startActivity(intent);
     }
 }
--- PersonalityApp/app/src/main/java/com/example/personalityapp/Question2.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Question2.java
@@ -9,16 +9,27 @@ import android.widget.Button;
 
 public class Question2 extends AppCompatActivity {
     private Button button;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question2);
+        setContentView(R.layout.activity_question2);                //set to the current page
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View view) {                        //define what to do on click
+                //do something
+                openQuestion3();                    //go to next question
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                openQuestion3();
+                //do something
+                openQuestion3();                    //go to next question
             }
         });
     }
