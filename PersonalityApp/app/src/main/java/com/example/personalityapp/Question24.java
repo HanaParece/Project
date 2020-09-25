f28e0be2ee91e88d9c47725d8eeb047c8ab23c8c
chungrs <54409983+chungrs@users.noreply.github.com>
2020-09-25 19:17:47 -0400

    fixed broken link found by ben

--- PersonalityApp/app/src/main/java/com/example/personalityapp/Question24.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Question24.java
@@ -1,13 +1,13 @@
 package com.example.personalityapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Question21 extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Question24 extends AppCompatActivity {
     private Button buttonA;                                     //declare buttonA as a button that will be defined
     private Button buttonB;
     @Override
@@ -20,7 +20,7 @@ public class Question21 extends AppCompatActivity {
             @Override
             public void onClick(View view) {                        //define what to do on click
                 //do something
-                openQuestion22();                    //go to next question
+                openQuestion25();                    //go to next question
             }
         });
 
@@ -29,14 +29,14 @@ public class Question21 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //do something
-                openQuestion22();                    //go to next question
+                openQuestion25();                    //go to next question
             }
         });
     }
 
 
-    public void openQuestion22(){
-        Intent intent = new Intent(this, Question22.class);
+    public void openQuestion25(){
+        Intent intent = new Intent(this, Question25.class);
         startActivity(intent);
     }
 }
