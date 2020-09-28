43842eca9562a6c4d6c111e52c149a4d4fd96062
Hodgeb2 <70536911+Hodgeb2@users.noreply.github.com>
2020-09-28 09:55:06 -0400

    Fixed label errors and as of now the app runs up to question 26

--- PersonalityApp/app/src/main/java/com/example/personalityapp/Question22.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Question22.java
@@ -13,7 +13,7 @@ public class Question22 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question20);                //set to the current page
+        setContentView(R.layout.activity_question22);                //set to the current page
 
         buttonA = (Button) findViewById(R.id.buttonA);
         buttonA.setOnClickListener(new View.OnClickListener() {
