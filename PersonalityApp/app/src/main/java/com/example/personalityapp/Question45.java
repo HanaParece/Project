adae457281c2a92da0ab3786ef4c2d84357f2929
Hodgeb2 <70536911+Hodgeb2@users.noreply.github.com>
2020-10-05 17:03:20 -0400

    Link and button up to 45

--- PersonalityApp/app/src/main/java/com/example/personalityapp/Question45.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Question45.java
@@ -1,14 +1,42 @@
 package com.example.personalityapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Question45 extends AppCompatActivity {
-
+    private Button buttonA;                                     //declare buttonA as a button that will be defined
+    private Button buttonB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question45);
+        setContentView(R.layout.activity_question45);                //set to the current page
+
+        buttonA = (Button) findViewById(R.id.buttonA);
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {                        //define what to do on click
+                //do something
+                openQuestion46();                    //go to next question
+            }
+        });
+
+        buttonB = (Button) findViewById(R.id.buttonB);
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do something
+                openQuestion46();                    //go to next question
+            }
+        });
+    }
+
+    //link goes back to first page for now
+    public void openQuestion46(){
+        Intent intent = new Intent(this, Question46.class); // change this line when adding more questions
+        startActivity(intent);
     }
 }
