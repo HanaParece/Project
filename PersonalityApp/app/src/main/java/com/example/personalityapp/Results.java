49da71ae216c96dba234dece39944fa70dfb1a87
chungrs <54409983+chungrs@users.noreply.github.com>
2020-10-09 23:49:25 -0400

    Added function to results page

--- PersonalityApp/app/src/main/java/com/example/personalityapp/Results.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Results.java
@@ -3,6 +3,7 @@ package com.example.personalityapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(MainActivity.profile.toString());
+
     }
 }
