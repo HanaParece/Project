00b66bc8387479f28b180d9c451ec10f6b57d941
Hodgeb2 <70536911+Hodgeb2@users.noreply.github.com>
2020-10-28 13:51:25 -0400

    Activated CheckStyles

--- PersonalityApp/app/src/main/java/com/example/personalityapp/Question1.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Question1.java
@@ -1,11 +1,11 @@
 package com.example.personalityapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class Question1 extends AppCompatActivity {
     private Button buttonA;                                     //declare buttonA as a button that will be defined
